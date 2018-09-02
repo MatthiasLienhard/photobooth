324b7270305cb9978d451de8b8acbf7961fc7c97
Matthias Lienhard <mali270484@gmail.com>
2018-09-02 10:07:43 +0200

    printer test

--- tests/printer.py
+++ tests/printer.py
@@ -35,19 +35,24 @@ else:
     raise Exception(printer_name + " not found")
 
 
-print_id=cups_conn.printFile(printer, list.get(0), " ", {})
+files=["/home/matthias/"]
 
-sleep(.1)
+#print_id=cups_conn.printFile(printer, list.get(0), " ", {})
+print_id=cups_conn.printFile(printer, files[0], " ", {})
+print_id=cups_conn.printFile(printer, files[1], " ", {})
+
 
 print("start printing...")
-attr = cups_conn.getPrinterAttributes(printer)
-printqueuelength = len(cups_conn.getJobs())
+while True:
+    attr = cups_conn.getPrinterAttributes(printer)
+    printqueuelength = len(cups_conn.getJobs())
 
-print(",".join(attr['printer-state-reasons']))#=['paused']
-print(attr['printer-state-message'])#: '',
-print(attr['queued-job-count'])##=2
-print(attr['printer-state'])##=5
-print(attr['printer-error-policy'])#: 'retry-job',
+    print(",".join(attr['printer-state-reasons']))#=['paused']
+    print(attr['printer-state-message'])#: '',
+    print(attr['queued-job-count'])##=2
+    print(attr['printer-state'])##=5
+    print(attr['printer-error-policy'])#: 'retry-job',
 
+    sleep(.1)
 
 
